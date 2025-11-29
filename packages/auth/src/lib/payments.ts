1b8d8a991a1f06f4977bce88f6804e7ec76ca9d3
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2025-11-29 17:32:13 +0100

    chore: fix payment issue and server type

--- packages/auth/src/lib/payments.ts
+++ packages/auth/src/lib/payments.ts
@@ -1,6 +1,10 @@
 import { Polar } from "@polar-sh/sdk";
 
 export const polarClient = new Polar({
-	accessToken: process.env.POLAR_ACCESS_TOKEN,
-	server: "sandbox",
+  accessToken: process.env.POLAR_ACCESS_TOKEN,
+  server:
+    process.env.POLAR_ENV === "production" ||
+    process.env.NODE_ENV === "production"
+      ? "production"
+      : "sandbox",
 });
